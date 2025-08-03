b2b947ec7e2c750f236d61cbdaf514247e8cacf3
andy-susanto <officialandysusanto@gmail.com>
2025-08-03 22:54:23 +0700

    pul

--- resources/views/barangmasuk/index.blade.php
+++ resources/views/barangmasuk/index.blade.php
@@ -55,7 +55,7 @@
                                         <td>{{ $data->nomor_surat_jalan }}</td>
                                         <td>{{ $data->nama_kapal }}</td>
                                         <td>{{ $data->nomor_container }}</td>
-                                        <td>{{ \Carbon\Carbon::parse($data->tanggal_keberangkatan)->format('d M Y') }}
+                                        <td>{{ \Carbon\Carbon::parse($data->tanggal_keberangkatan)->format('Y-m-d') }}
                                         </td>
                                         <td>{{ $data->nama_penerima_barangmasuk }}</td>
                                         <td>{{ $data->barang->nm_barang }}</td>
