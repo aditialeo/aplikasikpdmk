2d3ad910f25efd260aba8bed438166364a8d0b0a
andy-susanto <officialandysusanto@gmail.com>
2025-08-03 21:32:21 +0700

    update container barang masuk

--- resources/views/barangmasuk/index.blade.php
+++ resources/views/barangmasuk/index.blade.php
@@ -20,78 +20,80 @@
                             </button>
                         </div>
                     @endif
-                    {{-- Tombol tambah barang masuk --}}
-                    <a class="btn btn-primary float-right text-xs" href="{{ route('barangmasuk.create') }}" role="button">
-                        <i class="fa fa-plus-circle" aria-hidden="true"></i> Tambah Barang Masuk
-                    </a>
 
+                    {{-- Tombol tambah barang masuk --}}
+                    <a href="{{ route('barangmasuk.create') }}" class="btn btn-primary float-right text-xs">
+                        <i class="fa fa-plus-circle"></i> Tambah Barang Masuk
+                    </a>
                 </div>
 
                 <div class="card-body">
-                    <table class="table datatable">
-                        <thead>
-                            <tr>
-                                <th>NO</th>
-                                <th>Nomor Surat Jalan</th>
-                                <th>Nama Kapal</th>
-                                <th>Nomor Kontainer</th>
-                                <th>Tanggal Keberangkatan</th>
-                                <th>Nama Penerima</th>
-                                <th>Nama Barang</th>
-                                <th>Kode Barang</th>
-                                <th>Supplair</th>
-                                <th>Jumlah Masuk</th>
-                                <th>Nama Merk</th>
-                                <th>Tanggal Masuk</th>
-                                <th>Input Oleh</th>
-                                <th>#</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($barangmasuk as $data)
+                    <div class="table-responsive">
+                        <table class="table datatable w-100">
+                            <thead>
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $data->nomor_surat_jalan }}</td>
-                                    <td>{{ $data->nama_kapal }}</td>
-                                    <td>{{ $data->nomor_kontainer }}</td>
-                                    <td>{{ $data->tanggal_keberangkatan }}</td>
-                                    <td>{{ $data->nama_penerima_barangmasuk }}</td>
-                                    <td>{{ $data->barang->nm_barang }}</td>
-                                    <td>{{ $data->kd_barang }}</td>
-                                    <td>{{ optional($data->suplair)->nama_suplair ?? 'Tidak ada supplair' }}</td>
-                                    <td>{{ $data->jumlah_masuk }}</td>
-                                    <td>{{ $data->merk->nama }}</td>
-                                    <td>{{ $data->tanggal_masuk ? \Carbon\Carbon::parse($data->tanggal_masuk)->isoFormat('dddd, D MMMM YYYY') : '-' }}
-                                    </td>
-
-                                    <td>{{ optional($data->user)->name ?? 'Tidak diketahui' }}</td>
-
-                                    <td>
-                                        <div class="d-flex gap-1">
-                                            {{-- Tombol Edit --}}
-                                            @can('update-barangmasuk')
-                                                <a class="btn btn-primary text-xs mr-1"
-                                                    href="{{ route('barangmasuk.edit', $data->id) }}" role="button">Edit</a>
-                                            @endcan
-
-                                            {{-- Tombol Hapus --}}
-                                            @can('delete-barangmasuk')
-                                                <form action="{{ route('barangmasuk.destroy', $data->id) }}" method="post"
-                                                    onsubmit="return confirm('Anda yakin ingin menghapus item ini ?');">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="btn btn-danger text-xs">Hapus</button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </td>
+                                    <th>NO</th>
+                                    <th>Nomor Surat Jalan</th>
+                                    <th>Nama Kapal</th>
+                                    <th>Nomor Kontainer</th>
+                                    <th>Tanggal Keberangkatan</th>
+                                    <th>Nama Penerima</th>
+                                    <th>Nama Barang</th>
+                                    <th>Kode Barang</th>
+                                    <th>Supplair</th>
+                                    <th>Jumlah Masuk</th>
+                                    <th>Nama Merk</th>
+                                    <th>Tanggal Masuk</th>
+                                    <th>Input Oleh</th>
+                                    <th>Aksi</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
+                            <tbody>
+                                @foreach ($barangmasuk as $data)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $data->nomor_surat_jalan }}</td>
+                                        <td>{{ $data->nama_kapal }}</td>
+                                        <td>{{ $data->nomor_container }}</td>
+                                        <td>{{ $data->tanggal_keberangkatan }}</td>
+                                        <td>{{ $data->nama_penerima_barangmasuk }}</td>
+                                        <td>{{ $data->barang->nm_barang }}</td>
+                                        <td>{{ $data->kd_barang }}</td>
+                                        <td>{{ optional($data->suplair)->nama_suplair ?? 'Tidak ada supplair' }}</td>
+                                        <td>{{ $data->jumlah_masuk }}</td>
+                                        <td>{{ $data->merk->nama }}</td>
+                                        <td>
+                                            {{ $data->tanggal_masuk ? \Carbon\Carbon::parse($data->tanggal_masuk)->isoFormat('dddd, D MMMM YYYY') : '-' }}
+                                        </td>
+                                        <td>{{ optional($data->user)->name ?? 'Tidak diketahui' }}</td>
+                                        <td>
+                                            <div class="d-flex flex-wrap gap-1">
+                                                @can('update-barangmasuk')
+                                                    <a href="{{ route('barangmasuk.edit', $data->id) }}"
+                                                        class="btn btn-primary btn-sm mr-1">
+                                                        Edit
+                                                    </a>
+                                                @endcan
 
-            </div>
+                                                @can('delete-barangmasuk')
+                                                    <form action="{{ route('barangmasuk.destroy', $data->id) }}" method="POST"
+                                                        onsubmit="return confirm('Anda yakin ingin menghapus item ini?');">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger btn-sm">
+                                                            Hapus
+                                                        </button>
+                                                    </form>
+                                                @endcan
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div> {{-- /.table-responsive --}}
+                </div> {{-- /.card-body --}}
+            </div> {{-- /.card --}}
         </div>
     </div>
 @stop
@@ -102,7 +104,14 @@
 @section('js')
     <script>
         $(document).ready(function() {
-            $('.datatable').DataTable();
+            $('.datatable').DataTable({
+                responsive: true,
+                autoWidth: false,
+                scrollX: true,
+                language: {
+                    url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/id.json" // Jika ingin Bahasa Indonesia
+                }
+            });
         });
     </script>
 @stop
